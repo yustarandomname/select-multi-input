d15732b86ddae6fdd90dff71cff0e7a847799f1a
abel (tudelft) <a.debruijn-3@student.tudelft.nl>
2022-06-07 17:18:07 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let videoSource: HTMLVideoElement; // Video output source
+	let videoSelected: MediaDeviceInfo; // video output selected
+	let videoOptions: MediaDeviceInfo[] = []; // video ids as list
+
+	async function getStream() {
+		const deviceId = videoSelected ? { exact: videoSelected } : { facingMode: 'environment' };
+
+		const constraints = {
+			video: deviceId
+		};
+
+		console.log(JSON.stringify(constraints));
+
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia(constraints);
+			videoSource.srcObject = stream;
+			videoSource.play();
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	}
+
+	onMount(async () => {
+		const stream = await getStream();
+		const devices = await navigator.mediaDevices.enumerateDevices();
+		videoOptions = devices.filter((device) => device.kind == 'videoinput');
+		console.log(videoOptions.length);
+	});
+</script>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<video bind:this={videoSource} />
+<select bind:value={videoSelected} on:change={getStream}>
+	{#each videoOptions as option, index}
+		<option value={option}>
+			{option.label || 'Camera ' + (index + 1)}
+		</option>
+	{/each}
+</select>
